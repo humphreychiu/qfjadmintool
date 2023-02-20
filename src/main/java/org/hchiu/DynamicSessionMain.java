8e10166c38bb295d8e1b7fbf2ea42102c55f44a7
humphrey chiu <hchiu@trumidtech.com>
2023-02-20 16:55:22 +0000

    Correcting duplicate session protection

--- src/main/java/org/hchiu/DynamicSessionMain.java
+++ src/main/java/org/hchiu/DynamicSessionMain.java
@@ -18,16 +18,16 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
 
 public class DynamicSessionMain {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DynamicSessionMain.class);
     private static final String CONFIG_TEMPLATE = "acceptor_base.config";
     private static final ClassLoader MAIN_LOADER = DynamicSessionMain.class.getClassLoader();
+    private static final Map<String, Acceptor> targetCompIdAcceptors = new HashMap<>();
 
     public static void main(String[] args) throws Exception {
-
-        Map<String, Acceptor> targetCompIdAcceptors = new HashMap<>();
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         boolean isRun = true;
         LOGGER.info("Use command \"add abc\" to add a new client session abc.");
@@ -37,30 +37,19 @@ public class DynamicSessionMain {
             if (value != null) {
                 if (value.startsWith("add")) {
                     String client = value.split(" ")[1];
-                    var size = targetCompIdAcceptors.size();
-                    if (targetCompIdAcceptors.containsKey(targetCompIdAcceptors)) {
-                        LOGGER.warn("session {} already exists!", client);
-                    } else {
-                        targetCompIdAcceptors.put(client, addAcceptor(size, client));
-                    }
+                    targetCompIdAcceptors.computeIfAbsent(client, DynamicSessionMain::addAcceptor);
                 } else if (value.startsWith("deactivate")) {
                     String client = value.split(" ")[1];
-                    if (targetCompIdAcceptors.containsKey(client)) {
+                    consumeSession(client, (Session session) -> {
                         LOGGER.info("deactivating session {}", client);
-                        targetCompIdAcceptors.get(client).getSessions().forEach(sessionID -> Session.lookupSession(
-                                sessionID).logout("admin logout"));
-                    } else {
-                        LOGGER.warn("{} session not found", client);
-                    }
+                        session.logout("admin logout");
+                    });
                 } else if (value.startsWith("activate")) {
                     String client = value.split(" ")[1];
-                    if (targetCompIdAcceptors.containsKey(client)) {
+                    consumeSession(client, (Session session) -> {
                         LOGGER.info("activating session {}", client);
-                        targetCompIdAcceptors.get(client).getSessions().forEach(sessionID -> Session.lookupSession(
-                                sessionID).logon());
-                    } else {
-                        LOGGER.warn("{} session not found", client);
-                    }
+                        session.logon();
+                    });
                 } else if (value.startsWith("quit")) {
                     isRun = false;
                 }
@@ -70,26 +59,46 @@ public class DynamicSessionMain {
         System.exit(0);
     }
 
-    private static Acceptor addAcceptor(int n, String client) throws Exception {
-        String port = n < 10 ? "2000" + n : "200" + n;
-
-        AcceptorApp application = new AcceptorApp();
-        SessionSettings settings = new SessionSettings(MAIN_LOADER.getResourceAsStream(CONFIG_TEMPLATE));
-        var defaultProp = settings.getDefaultProperties();
-        settings.setString(  // triggers internal getOrCreateSession
-                new SessionID(defaultProp.getProperty("BeginString"), defaultProp.getProperty("SenderCompID"), client),
-                "SocketAcceptPort", port);
-        MessageStoreFactory storeFactory = new FileStoreFactory(settings);
-        LogFactory logFactory = new FileLogFactory(settings);
-        MessageFactory messageFactory = new DefaultMessageFactory();
-        Acceptor acceptor = new SocketAcceptor(application, storeFactory, settings, logFactory, messageFactory);
-
-        acceptor.start();
-        synchronized (DynamicSessionMain.class) {
-            Thread.sleep(1000);  //wait for acceptor starting asynchronously
+    private static void consumeSession(String client, Consumer<Session> consumer) {
+        if (targetCompIdAcceptors.containsKey(client)) {
+            targetCompIdAcceptors.get(client).getSessions()
+                    .forEach(sessionID -> consumer.accept(Session.lookupSession(sessionID)));
+        } else {
+            LOGGER.warn("{} session not found", client);
         }
-        LOGGER.info("starting acceptor for {} on port {}", acceptor.getSessions().get(0).getTargetCompID(), port);
+    }
 
-        return acceptor;
+    private static Acceptor addAcceptor(String client) throws RuntimeException {
+        try {
+            int n = targetCompIdAcceptors.size();
+            String port = n < 10 ? "2000" + n : "200" + n;
+
+            AcceptorApp application = new AcceptorApp();
+            SessionSettings settings = new SessionSettings(MAIN_LOADER.getResourceAsStream(CONFIG_TEMPLATE));
+            addSession(settings, client, port);
+            MessageStoreFactory storeFactory = new FileStoreFactory(settings);
+            LogFactory logFactory = new FileLogFactory(settings);
+            MessageFactory messageFactory = new DefaultMessageFactory();
+            Acceptor acceptor = new SocketAcceptor(application, storeFactory, settings, logFactory, messageFactory);
+
+            acceptor.start();
+            synchronized (DynamicSessionMain.class) {
+                Thread.sleep(1000);  //wait for acceptor starting asynchronously
+            }
+            LOGGER.info("starting acceptor for {} on port {}", acceptor.getSessions().get(0).getTargetCompID(), port);
+
+            return acceptor;
+        } catch (Exception exp) {
+            throw new RuntimeException("failed to add acceptor", exp);
+        }
+    }
+
+    private static void addSession(SessionSettings settings, String client, String port) {
+        var defaultProp = settings.getDefaultProperties();
+        var sessionId = new SessionID(
+                defaultProp.getProperty("BeginString"),
+                defaultProp.getProperty("SenderCompID"),
+                client);
+        settings.setString(sessionId, "SocketAcceptPort", port); // triggers internal getOrCreateSession
     }
 }
